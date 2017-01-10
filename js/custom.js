50e38c85773fc9914c6695f8351887bcc0d5fc09
Ravi Garbuja Pun <Ravi Garbuja Pun>
2017-01-11 03:11:09 +0545

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/*function onHoverAnimation(){
+	$('fa').hover(
+		$('this').removeClass('fa-twitter fa-4x');
+		$('this').addClass('fa-twitter fa-5x');
+	)
+}
+*/
+
+
+$(document).ready(function(){
+	$(function() {
+    $('#main').animate({opacity: '1', height: '100%', width: '100%' }, 1000, 'linear');
+});
+
+})
